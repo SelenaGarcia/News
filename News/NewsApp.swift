73d4f45ad3d0a60f78cdbe4b85dde8b7bed68c15
SelenaGarcia <selenagarcialobo@gmail.com>
2022-07-23 17:21:41 -0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Selena Belén Garcia Lobo on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
